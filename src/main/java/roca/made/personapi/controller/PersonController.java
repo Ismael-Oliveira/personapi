db14d760cf1944191447cc57522dadecfa2d6862
Ismael-Oliveira <oliveiraismael2@gmail.com>
2020-11-12 11:01:12 -0300

    first commit

---
+++ src/main/java/roca/made/personapi/controller/PersonController.java
@@ -0,0 +1,22 @@
+package roca.made.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook() {
+        return "API Rest";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable int id) {
+
+        return "Get BY id: " + id;
+    }
+}
---
+++ src/test/java/roca/made/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package roca.made.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
